41b8c4b5bd589d0649728b40596e3d01cf8542cc
Shafi Masoumi <masoumi2017@icloud.com>
2019-11-23 10:57:42 -0800

    actions and components created

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+
 class App extends Component {
+  state = {
+    name: '',
+    age: '',
+    height:''
+  }
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
+        <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
-      </div>
+        </div>
     );
   }
 }
