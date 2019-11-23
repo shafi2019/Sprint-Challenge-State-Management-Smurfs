de26c4353b642baf7ac611721fb515725c0cc102
Shafi Masoumi <masoumi2017@icloud.com>
2019-11-23 11:04:58 -0800

    MVP Done App is working

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+import Smurfs from './smurfs'
+import {connect} from 'react-redux';
+import {fetchSmurfs} from '../actions/index'
+import SmurfForm from './smurfForm'
+
 
 class App extends Component {
   state = {
@@ -14,9 +19,20 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <Smurfs smurfs ={this.state.smurfs}/>
+        <SmurfForm addSmurf = {this.props.handleSubmit}/>
         </div>
     );
   }
 }
 
-export default App;
+
+ const mapStateToProps = state => {
+  return {
+      smurfs: state.smurfs
+  }
+}
+export default connect (
+  mapStateToProps,
+  {fetchSmurfs}
+)(App)
